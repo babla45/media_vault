50f4bf7c1ec3f8c250117f5a63ced9baf7bd8a27
Md Babla Islam <bilasislam5643@gmail.com>
2026-05-05 12:19:47 +0600

    proguard added, now only 2mb app size which was previously 26 mb

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,19 +15,25 @@ android {
         applicationId = "com.example.bib_vault"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
     compileOptions {
